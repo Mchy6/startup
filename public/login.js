5d1db24e13912af3ab5246ab361a44e8693d7ec9
Matthew Bailey <mb@Matthews-MacBook-Air.local>
2023-12-04 11:02:47 -0700

    login fixed

--- public/login.js
+++ public/login.js
@@ -1,7 +1,79 @@
-function login() {
-  const nameEl = document.querySelector("#username");
-  const passEl = document.querySelector("#password");
-  localStorage.setItem("username", nameEl.value);
-  localStorage.setItem("password", passEl.value);
-  // window.location.href = "play.html";
+/*  
+  Where I'm at:
+    Code looks the same as simon-login, but it's not sending to mongodb
+    deploy service error: "stdin is not a terminal". 
+*/
+
+(async () => {
+  const username = localStorage.getItem('username');
+  if (username) {
+    document.querySelector('#playerName').textContent = `Signed in as ${username}`;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  console.log('createUser');
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  console.log('loginOrCreate');
+  const username = document.querySelector('#username')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: username, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('username', username);
+    window.location.href = 'learn.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
+
+function play() {
+  window.location.href = 'learn.html';
+}
+
+function logout() {
+  localStorage.removeItem('username');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
 }
